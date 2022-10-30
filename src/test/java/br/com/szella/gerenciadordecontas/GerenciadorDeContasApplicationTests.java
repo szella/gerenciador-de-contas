0cec2f8eba3e93be0d7af95f574e0b8225073b64
Sergio Zella <sergioluiz.zella@gmail.com>
2022-10-30 10:59:27 -0300

    Primeiro commit

---
+++ src/test/java/br/com/szella/gerenciadordecontas/GerenciadorDeContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.szella.gerenciadordecontas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
