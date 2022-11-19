c983d54a858e11ebf32f798fd72e8997e556aeb9
Sergio Zella <sergioluiz.zella@gmail.com>
2022-11-19 16:10:24 -0300

    mudando configuracao do cache em redis

--- src/main/java/br/com/szella/gerenciadordecontas/GerenciadorDeContasApplication.java
+++ src/main/java/br/com/szella/gerenciadordecontas/GerenciadorDeContasApplication.java
@@ -2,14 +2,12 @@ package br.com.szella.gerenciadordecontas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
 @SpringBootApplication
 public class GerenciadorDeContasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorDeContasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorDeContasApplication.class, args);
+    }
 
 }
